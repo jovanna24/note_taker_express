8460cb5a880168adb34974adabc0b90bc6e2f43f
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-05-15 11:48:56 -0700

    final edits, fixed appication

--- index.js
+++ index.js
@@ -8,31 +8,50 @@ const app = express();
 const PORT = process.env.PORT || 3001; 
 
 // Import middleware for parsing JSON 
-app.use(express.json()); 
+app.use(express.json());  
+app.use(express.static('public'));
 
 // GET Route for the notes page 
 app.get('/notes', (req, res)=> {
-    res.sendFile(path.join(__dirname, '/api/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 }); 
 
 // GET Route for the homepage 
-app.get('*', (req, res)=> {
-    res.sendFile(path.join(__dirname, '/api/index.html'));
-}); 
+
+
+// app.get('/assets/css/styles.css', (req, res)=> {
+//     res.sendFile(path.join(__dirname, '/public/assets/css/styles.css'));
+// });
 
 // API Routes 
 app.get('/api/notes', (req, res)=> {
-    fs.readFile(path.join(__dirname, 'db.json'), 'utf-8', (err, data)=>{
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf-8', (err, data)=>{
         if (err) {
             console.error(err); 
             res.status(500).json({error: 'Internal Server Error' });
             return;
         }
         const notes = JSON.parse(data); 
+        res.json(notes);
+    });
+}); 
+
+app.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+}); 
+
+app.post('/api/notes', (req, res)=>{
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf-8', (err, data)=>{
+        if (err) {
+            console.error(err); 
+            res.status(500).json({error: 'Internal Server Error'}); 
+            return;
+        }
+        const notes = JSON.parse(data); 
         const newNote = req.body; 
-        newNote.id = Date.now(); 
-        notes.push(newNote);  
-        fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2), (err) => {
+        newNote.id = Date.now();  
+        notes.push(newNote); 
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes, null, 2), (err) => {
             if (err) {
                 console.error(err); 
                 res.status(500).json({error: 'Internal Server Error'}); 
@@ -42,6 +61,29 @@ app.get('/api/notes', (req, res)=> {
         });
     });
 }); 
+
+app.delete("/api/notes/:id", (req, res)=> {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf-8', (err, data)=>{
+        if (err) {
+            console.error(err); 
+            res.status(500).json({error: 'Internal Server Error'}); 
+            return;
+        }
+        const notes = JSON.parse(data); 
+        
+        const idToDelete = req.params.id;
+        const result = notes.filter((note) => note.id != idToDelete);
+        
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(result, null, 2), (err) => {
+            if (err) {
+                console.error(err); 
+                res.status(500).json({error: 'Internal Server Error'}); 
+                return;
+            }
+            res.json(result);
+        });
+    });
+})
 
 // Listen for incoming connection on the specified port 
 app.listen(PORT, ()=>{
